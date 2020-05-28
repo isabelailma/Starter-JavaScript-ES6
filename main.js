dba0ed06e9e5eaa076798ba6281b0698003ab346
Isabela Ilma Gonçalves <isabela.ilma@hotmail.com>
2020-05-28 12:13:07 -0300

    Template literals

--- main.js
+++ main.js
@@ -1,52 +1,5 @@
-// REST
-// Objeto
-const usuario = {
-	nome: 'Isabela',
-	idade: 26,
-	empresa: 'GM',
-}
+const nome = 'Isabela';
+const idade = 26;
 
-const {nome, ...resto} = usuario;
-
-console.log(nome);
-console.log(resto);
-
-// Vetor
-const arr = [1, 2, 3, 4];
-
-const [a, b, ...c] = arr;
-
-console.log(a);
-console.log(b);
-console.log(c);
-
-// Parâmetro de função
-function soma(a, b, c) {
-	return a + b + c;
-}
-
-console.log(soma(1, 3, 4));
-
-function soma2(...params) {
-	return params.reduce((soma, next) => soma + next);
-}
-
-console.log(soma2(1, 3, 4, 7));
-
-// SPREAD
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-
-const arr3 = [...arr1, ...arr2];
-
-console.log(arr3);
-
-const usuario1 = {
-	nome: 'Isabela',
-	idade: 26,
-	empresa: 'GM',
-};
-
-const usuario2 = { ...usuario1, nome: 'Marcus'};
-
-console.log(usuario2);
+console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos.');
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`);
