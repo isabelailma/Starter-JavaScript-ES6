70dd8b68d8fdea5be2aeb7d2dce64c4217ccde7b
Isabela Ilma Gonçalves <isabela.ilma@hotmail.com>
2020-05-28 13:23:55 -0300

    Resolução 2º Exercício Módulo 01

---
+++ Módulo 01/02.js
@@ -0,0 +1,37 @@
+/** Exercício 2 - A partir do seguinte vetor e utilizando os métodos de array (map, reduce, 
+ * filter e find): */
+
+const usuarios = [
+  { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },
+  { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
+  { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
+ ];
+
+// 2.1 MAP - Crie uma variável que contenha todas idades dos usuários: [23, 15, 30]
+const map = usuarios.map(({idade}) => idade);
+
+console.log(map);
+
+// 2.2 FILTER - Crie uma variáveis que tenha apenas os usuários que trabalham na Rocketseat e com 
+// mais de 18 anos: [{ nome: 'Diego', idade: 23, empresa: 'Rocketseat' }]
+const filter = usuarios.filter(user => user.empresa === 'Rocketseat' && user.idade >= 18);
+
+console.log(filter);
+
+// 2.3 FIND - Crie uma variável que procura por um usuário que trabalhe na empresa Google: undefined
+const find = usuarios.find(({empresa}) => empresa === 'Google');
+
+console.log(find);
+
+// 2.4 - Multiplique a idade de todos usuários por dois e depois realize um filtro nos 
+// usuários que possuem no máximo 50 anos:
+// Resultado:
+// [
+//   { nome: 'Diego', idade: 46, empresa: 'Rocketseat' },
+//   { nome: 'Gabriel', idade: 30, empresa: 'Rocketseat' },
+// ]
+const idadeX2 = usuarios.map(user => ({...user, idade: user.idade * 2}));
+console.log(idadeX2);
+
+const user50 = idadeX2.filter(user => user.idade <= 50);
+console.log(user50);
