339bb053dc5c85cbfdf73c0a9e15eecb6dd8e64f
Isabela Ilma Gonçalves <isabela.ilma@hotmail.com>
2020-05-29 13:14:05 -0300

    Promise

--- src/main.js
+++ src/main.js
@@ -1,27 +1,7 @@
-/* Exercicio 1 - Todos os exercícios abaixo necessitam que você esteja com o Webpack configurado.
+const minhaPromise = () => new Promise((resolve, reject) => {
+  setTimeout(() => {resolve('OK')}, 2000);
+});
 
-Crie um arquivo chamado functions.js com o seguinte conteúdo:
-
-export const idade = 23;
-
-export default class Usuario {
-  static info() {
-    console.log('Apenas teste');
-  }
-}
-
-1.1 Agora em seu arquivo principal import apenas a classe Usuario renomeando-a para ClasseUsuario
-e chame a função info(); */
-
-import ClasseUsuario from './functions';
-
-console.log(ClasseUsuario.info());
-
-/* 1.2 Em seu arquivo principal importe a variável de idade e exiba a mesma em tela; */
-import {idade} from './functions';
-
-console.log(idade);
-
-/* 1.3 Em seu arquivo principal importe tanto a classe quanto a variável idade e renomeie a variável 
-idade para IdadeUsuario. */
-import Usuario, {idade as IdadeUsuario} from './functions';
+minhaPromise().then(response => {
+  console.log(response);
+});
