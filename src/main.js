f65e4499a0042cddfef0ac40046afe9c23e8d359
Isabela Ilma Gonçalves <isabela.ilma@hotmail.com>
2020-05-29 14:40:48 -0300

    Started Axios

--- src/main.js
+++ src/main.js
@@ -1,39 +1,15 @@
-const minhaPromise = () => new Promise((resolve, reject) => {
-  setTimeout(() => {resolve('OK')}, 2000);
-});
+import axios from 'axios';
 
-// minhaPromise()
-//   .then(response => {
-//     console.log(response);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
+class Api {
+  static async getUserInfo(username) {
+    try {
+      const response = await axios.get(`https://api.github.com/users/${username}`);
+      console.log(response);
+    }catch (err) {
+      console.warn('⚠ Usuário inexistente!');
+    }
+  }
+}
 
-// async function executaPromise() {
-//   minhaPromise().then(response => {
-//     console.log(response);
-//
-//     minhaPromise().then(response => {
-//       console.log(response);
-//
-//       minhaPromise().then(response => {
-//         console.log(response);
-//       })
-//     })
-//   })
-// }
-
-// async function executaPromise() {
-//   console.log(await minhaPromise());
-//   console.log(await minhaPromise());
-//   console.log(await minhaPromise());
-// }
-
-const executaPromise = async () => {
-  console.log(await minhaPromise());
-  console.log(await minhaPromise());
-  console.log(await minhaPromise());
-};
-
-executaPromise();
+Api.getUserInfo('isabelailma');
+Api.getUserInfo('isabelailmaa');
