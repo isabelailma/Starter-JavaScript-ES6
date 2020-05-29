df6e8a6a1de678828b2e191bb5a6fe8110ee4582
Isabela Ilma Gonçalves <isabela.ilma@hotmail.com>
2020-05-29 13:35:09 -0300

    Async/await

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ const minhaPromise = () => new Promise((resolve, reject) => {
   setTimeout(() => {resolve('OK')}, 2000);
 });
 
-minhaPromise().then(response => {
-  console.log(response);
-});
+// minhaPromise()
+//   .then(response => {
+//     console.log(response);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+async function executaPromise() {
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+}
+
+executaPromise();
